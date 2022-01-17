cd571019a49db31adc9078ab5c1d6dc668fb862e
Blood-Blitz <seelenbrecher@outlook.com>
2022-01-17 11:00:33 -0600

    'Add three arrays to mixed.js'

---
+++ mixed.js
@@ -0,0 +1,3 @@
+let weatherArr = ['Sunny', 'Cloudy', 'Rainy', 'Snowy'];
+let luckArr = ['Best of luck', 'Sorta good', 'Kinda bad', 'Worst luck'];
+let shouldDo = ['Watch out for spiders', 'Don\'t trust clowns today', 'Help the purple man']
